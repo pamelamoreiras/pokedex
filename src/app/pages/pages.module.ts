a4726170f314e0c208acdfecdd61d67fc6c45832
pamelamoreiras <pamelamoreiras99@gmail.com>
2023-08-08 11:29:13 -0300

    Base of Pages

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Module Routing
+import { RoutingModule } from './routing.module';
 
+//Pages
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    DetailsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule
   ]
 })
 export class PagesModule { }
